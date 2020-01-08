4d8eb736baf2a4dc3a22fe8515710c362227305b
pnemeth <pnemeth@LT000305>
2020-01-08 17:20:37 +0100

    Restore master

--- test/Program.cs
+++ test/Program.cs
@@ -8,39 +8,7 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-
-            myClass Person = new myClass("Béla");
-            Console.WriteLine($"Hello {Person.name}");
-
-            myClass Person2 = new myClass("Sanyi", 40);
-            Console.WriteLine($"Hello {Person2.name}");
-
-            string mi_a_fasz = "      Mi a fasz       ";
-            Console.WriteLine($"[{mi_a_fasz.ToUpper()}]");
-            Console.WriteLine($"[{mi_a_fasz.ToLower()}]");
-
-            mi_a_fasz = mi_a_fasz.Replace("fasz", "geci?");
-            Console.WriteLine($"[{mi_a_fasz}]");
         }
     }
 
-    class myClass: Program
-    {
-        public string name;
-
-        public myClass(string name)
-        {
-            this.name = name;
-            this.Age = 10;
-            Console.WriteLine($"{this.name} age is {this.Age}");
-            Console.WriteLine($"Length of this.name is {this.name.Length}");
-        }
-
-        public myClass(string name, byte age)
-        {
-            this.name = name;
-            this.Age = age;
-            Console.WriteLine($"{this.name} age is {this.Age}");
-        }
-    }
 }

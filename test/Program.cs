8c5bde781acf077d805ba4393745ea7c6e8c7846
pnemeth <pnemeth@LT000305>
2020-01-09 15:24:59 +0100

    Passed version

--- test/Program.cs
+++ test/Program.cs
@@ -1,14 +1,68 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace test
+class Solution
 {
-    class Program
+
+    // Complete the countingValleys function below.
+    static int countingValleys(string s)
     {
-        protected byte Age;
-        static void Main(string[] args)
+        int No_of_Valleys = 0;
+        int level = 0;
+        int marker = 0;
+
+        foreach (char c in s)
         {
-            Console.WriteLine("Hello");
+            if (c == 'U')
+            {
+                level++;
+            }
+            else if (c == 'D')
+            {
+                level--;
+            }
+            else 
+            {
+                Console.WriteLine("Wrong sequence has been added.");
+                break;
+            }
+            if ((level < 0) && (marker == 0))
+            {
+                No_of_Valleys++;
+                if (marker == 0) marker = 1;
+            }
+            else if (level == 0)
+            {
+                marker = 0;
+            }
+            //Console.WriteLine("level: " + level);
+            //Console.WriteLine("marker: " + marker);
         }
+
+        //Console.WriteLine("level: " + level);
+        //Console.WriteLine("No. of valleys: " + No_of_Valleys);
+        return No_of_Valleys;
     }
 
+    static void Main(string[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(s);
+        Console.WriteLine(result);
+    }
 }

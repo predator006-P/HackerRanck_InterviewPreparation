b2498012b1e213cd9e3c8f6d468f0b1c54330cd6
pnemeth <pnemeth@LT000305>
2020-01-21 15:28:04 +0100

    done

--- test/Program.cs
+++ test/Program.cs
@@ -1,14 +1,61 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace test
+class Solution
 {
-    class Program
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr)
     {
-        protected byte Age;
-        static void Main(string[] args)
+        int sum;
+        int max = -64;
+        for(int i = 0; i < 4; i++) 
         {
-            Console.WriteLine("Hello");
+            for(int j = 0; j < 4; j++) 
+            {
+                sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                if(sum > max)
+                {
+                    max = sum;
+                }
+            }
         }
+        if ((max > 63) && (max < -63))
+        {
+            Console.WriteLine("Wrong parameters have been added.");
+            return 0;
+        }
+
+        return max;
     }
 
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = hourglassSum(arr);
+
+        Console.WriteLine(result);
+
+    }
 }
+
+
